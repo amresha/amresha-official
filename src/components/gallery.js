4312d15245fe7f3fff846302411bf3c3793771e9
emmadigital <emma.digital.official@gmail.com>
2020-07-17 12:04:10 +0300

    update photo gallery code

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,96 +1,68 @@
-import React, { useState } from 'react'
-import Gallery from 'react-photo-gallery'
-import Carousel, { Modal, ModalGateway } from 'react-images'
-import styled from 'styled-components'
-import Img from 'gatsby-image'
+import React, { useState } from "react"
+import { graphql, StaticQuery } from "gatsby"
+import ThumbGrid from "./thumnails"
+import LightBox from "./lightbox"
+import { Grid } from "@material-ui/core"
 
-const ImageWrapper = styled.div`
-  box-shadow: -1px 3px 6px 1px rgba(0, 0, 0, 0.3);
-  transition: all 0.2s ease-in-out;
-  border-radius: 2px;
-  overflow: hidden;
-  cursor: zoom-in;
-  div {
-    transition: transform 2s;
+const Gallery = props => {
+  const [showLightbox, setShowLightbox] = useState(false)
+  const [selectedImage, setSelectedImage] = useState(null)
+
+  const handleOpen = i => e => {
+    setShowLightbox(true)
+    setSelectedImage(i)
   }
-  :hover {
-    box-shadow: -2px 5px 8px 2px rgba(0, 0, 0, 0.3);
-    div {
-      transform: scale(1.05);
-    }
+  const handleClose = () => {
+    setShowLightbox(false)
+    setSelectedImage(null)
   }
-`
-
-const GatsbyImage = ({ index, onClick, photo, margin }) => (
-  <ImageWrapper
-    style={{ margin, height: photo.height, width: photo.width }}
-    onClick={e => onClick(e, { index, photo })}
-    key={photo.key}
-  >
-    <Img
-      fixed={typeof window === 'undefined' ? { src: {} } : undefined}
-      fluid={photo.fluid}
-    />
-  </ImageWrapper>
-)
-
-const fileNumber = file =>
-    Number(file.node.childImageSharp.fluid.originalName.replace(/[a-z]/gi, ''))
-   
-    
-const getImages = imageArray => {
-  return [...imageArray]
-    .sort((a, b) => fileNumber(b) - fileNumber(a))
-    .map(({ node: { childImageSharp: { fluid, original } } }) => ({
-      height: original.height,
-      width: original.width,
-      src: fluid.originalImg,
-      srcSet: fluid.srcSet,
-      fluid,
-      key: fluid.originalName,
-    }))
-}
-
-const styleFn = styleObj => ({ ...styleObj, zIndex: 100 })
-
-const KnifeGallery = ({ photos, ...rest }) => {
-  const [isOpen, setOpen] = useState(false)
-  const [current, setCurrent] = useState(0)
-  const images = getImages(photos)
-
-  const imageClick = (e, obj) => {
-    setCurrent(obj.index)
-    setOpen(true)
+  const handlePrevRequest = (i, length) => e => {
+    setSelectedImage((i - 1 + length) % length)
   }
-
+  const handleNextRequest = (i, length) => e => {
+    setSelectedImage((i + 1) % length)
+  }
   return (
-    <div style={{ margin: '4rem auto' }}>
-      {photos.length > 1 && (
-        <Gallery
-          photos={images}
-          onClick={imageClick}
-          renderImage={GatsbyImage}
-          targetRowHeight={250}
-          margin={5}
-          {...rest}
-        />
-      )}
-
-      <ModalGateway>
-        {isOpen ? (
-          <Modal
-            onClose={() => {
-              setCurrent(0)
-              setOpen(false)
-            }}
-            styles={{ blanket: styleFn, positioner: styleFn }}
-          >
-            <Carousel views={images} currentIndex={current} />
-          </Modal>
-        ) : null}
-      </ModalGateway>
-    </div>
+    <StaticQuery
+      query={graphql`
+        query allImgQuery {
+          source:  allFile (filter: {absolutePath: {regex: "E:/amresha/amresha/src/images/gallery/"}}) {
+            edges {
+              node {
+                childImageSharp {
+                  original {
+                    width
+                    height
+                  }
+                  fluid {
+                    ...GatsbyImageSharpFluid
+                    originalName
+                    originalImg
+                  }
+                }
+              }
+            }
+          }
+        }
+      `}
+      render={data => {
+        const images = data.source.edges
+        return (
+          <Grid container spacing={24} justify="center">
+            <ThumbGrid images={images} handleOpen={handleOpen} />
+            {showLightbox && selectedImage !== null && (
+              <LightBox
+                images={images}
+                handleClose={handleClose}
+                handleNextRequest={handleNextRequest}
+                handlePrevRequest={handlePrevRequest}
+                selectedImage={selectedImage}
+              />
+            )}
+          </Grid>
+        )
+      }}
+    />
   )
 }
-
-export default KnifeGallery
+export default Gallery
